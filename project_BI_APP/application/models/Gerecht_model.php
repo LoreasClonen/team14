ad3f9b6f793ec5f60c91e138026bb0578a58635f
LoreasClonen <r0720792@student.thomasmore.be>
2019-02-21 14:51:51 +0100

    gerecht_model gemaakt + doxygen

---
+++ project_BI_APP/application/models/Gerecht_model.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Loreas
+ * Date: 21/02/2019
+ * Time: 14:42
+ */
+
+class Gerecht_model extends CI_Model
+{
+    /**
+     * @fn functie constructor Gerecht_model()
+     * @brief constructor voor de klasse Gerecht_model
+     * @post Er is een Gerecht_model klasse aangemaakt
+     *
+     */
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * @fn functie getAllByID()
+     * @brief geeft alle gerechten terug in de gerecht tabel
+     * @pre Er bestaat een Gerecht_model klasse
+     * @post Er is een array met 0 of meerdere gerechten teruggegeven
+     * @return array
+     */
+    function getAllById()
+    {
+        $this->db->order_by('id', 'asc');
+        $query = $this->db->get('Gerecht');
+        return $query->result();
+    }
+
+}
+
