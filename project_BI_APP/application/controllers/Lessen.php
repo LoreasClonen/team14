fe76db767dcaf7b7885e2e28f31c6165c80b7a0b
Steven <r0709457@student.thomasmore.be>
2019-02-28 14:41:42 +0100

    aanmaken lessen controller

---
+++ project_BI_APP/application/controllers/Lessen.php
@@ -0,0 +1,15 @@
+<?php
+
+    /**
+     * @class Lessen
+     * @brief Controller-klasse voor lessen
+     *
+     * Controller klasse met alle methodes die gebruikt worden voor alles wat te maken heeft met inloggers, zwemlessen en klanten
+     */
+    class Lessen extends CI_Controller
+    {
+        function herstelWachtwoord()
+        {
+            
+        }
+    }
