7685899fdd34f396c8c3ac7b10ad2e4ec20ac501
Steven <r0709457@student.thomasmore.be>
2019-03-07 12:35:45 +0100

    test

--- project_BI_APP/application/models/Lessen/Inlogger_model.php
+++ project_BI_APP/application/models/Lessen/Inlogger_model.php
@@ -1,32 +1,33 @@
-<?php
-/**
- * @Class Inlogger_Model
- */
-
-class Inlogger_model extends CI_Model
-{
+5555555555555<?php
     /**
-     * functie constructor Inlogger_model()
-     * @brief constructor voor de klasse Inlogger_model
-     * @post Er is een Inlogger_model klasse aangemaakt
-     *
+     * @Class Inlogger_Model
      */
-    function __construct()
-    {
-        parent::__construct();
-    }
 
-    /**
-     * functie getAllByID()
-     * @brief geeft alle inloggers terug in de inlogger tabel
-     * @pre Er bestaat een Inlogger_model klasse
-     * @post Er is een array met 0 of meerdere inlogger teruggegeven
-     * @return array
-     */
-    function getAllById()
+    class Inlogger_model extends CI_Model
     {
-        $this->db->order_by('id', 'asc');
-        $query = $this->db->get('Inlogger');
-        return $query->result();
+        /**
+         * functie constructor Inlogger_model()
+         * @brief constructor voor de klasse Inlogger_model
+         * @post Er is een Inlogger_model klasse aangemaakt
+         *
+         */
+        function __construct()
+        {
+            parent::__construct();
+        }
+
+        /**
+         * functie getAllByID()
+         * @brief geeft alle inloggers terug in de inlogger tabel
+         * @pre Er bestaat een Inlogger_model klasse
+         * @post Er is een array met 0 of meerdere inlogger teruggegeven
+         * @return array
+         */
+        function getAllById()
+        {
+            $this->db->order_by('id', 'asc');
+            $query = $this->db->get('Inlogger');
+            return $query->result();
+        }
+        
     }
-}
