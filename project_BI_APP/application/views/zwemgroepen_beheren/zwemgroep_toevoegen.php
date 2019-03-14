20e4b2672927ac9a4073881c4cda782d9526da4c
SReggers <greenskyro@gmail.com>
2019-03-14 12:27:13 +0100

    finished zwemgroep_toevoegen form

--- project_BI_APP/application/views/zwemgroepen_beheren/zwemgroep_toevoegen.php
+++ project_BI_APP/application/views/zwemgroepen_beheren/zwemgroep_toevoegen.php
@@ -24,7 +24,43 @@
         'donderdag' => 'Donderdag',
         'vrijdag' => 'Vrijdag',
     );
-    echo form_dropdown('weekdagen', $dataWeekdag);
+    echo form_dropdown('weekdag', $dataWeekdag);
+
+    echo form_label('Zwemniveau', 'zwemniveau');
+    $dataZwemniveau = array(
+            // Hier moeten de zwemniveaus ingelezen worden
+    );
+    echo form_dropdown('zwemniveau', $dataZwemniveau);
+
+    echo form_label('Max. Grootte', 'maxGrootte');
+    $dataGrootte = array(
+        'id' => 'maxGrootte',
+        'name' => 'maxGrootte',
+        'class' => 'form-control',
+        'placeholder' => 'grootte',
+        'required' => 'required',
+        'size' => '2');
+    echo form_input($dataGrootte);
+
+    echo form_label('Beginuur', 'beginuur');
+    $dataBeginuur = array(
+        'id' => 'beginuur',
+        'name' => 'beginuur',
+        'class' => 'form-control',
+        'required' => 'required',
+        'size' => '5');
+    echo form_input($dataBeginuur);
+
+    echo form_label('Einduur', 'einduur');
+    $dataEinduur = array(
+        'id' => 'einduur',
+        'name' => 'einduur',
+        'class' => 'form-control',
+        'required' => 'required',
+        'size' => '5');
+    echo form_input($dataEinduur);
+
+    echo form_hidden('inlogger', ''/*inloggerId hier*/);
 
     echo form_submit(array("value" => "Toevoegen", "class" => "btn btn-primary my-3", "id" => "voegZwemgroepToe"));
     echo form_close();
