61a5f72d3dc9ecec5209e2274587b54cfd8e43e8
LoreasClonen <r0720792@student.thomasmore.be>
2019-03-27 12:53:19 +0100

    keuze inschrijving

---
+++ project_BI_APP/application/views/zwemlessen/keuze_inschrijvingen.php
@@ -0,0 +1,5 @@
+<div class="container">
+    <?php echo smallDivAnchor('zwemlessen/index', 'Voogd', 'class="btn btn-primary"'); ?>
+    <?php echo smallDivAnchor('zwemlessen/', 'Kind', 'class="btn btn-primary"'); ?>
+
+</div>
