6517b071bc92b58d41a6b55887fd1e04d6266b38
sharinuyts <r0614779@student.thomasmore.be>
2019-04-03 10:33:58 +0200

    zwemfeestje annuleren mogelijk maken

--- project_BI_APP/application/views/overzicht_zwemfeestjes/zwemfeestje_annuleren.php
+++ project_BI_APP/application/views/overzicht_zwemfeestjes/zwemfeestje_annuleren.php
@@ -1,5 +1,23 @@
-<div class="col-12 mb-2">
-<?php
+<div class="container text-center">
+    <div class="row">
+        <div class="col-lg-4"></div>
+        <div class="col-lg-4">
+            <div class="card text-center">
+                <div class="card-header">
+                    Weet u het zeker?
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Uw zwemfeestje zal geannuleerd worden, weet u het zeker?</p>
 
-?>
+                </div>
+                <div class="card-footer text-muted">
+                    <?php
+                        echo anchor('Zwemfeestjes/deleteZwemfeestje/' . $zwemfeestId, 'Bevestigen', 'class="btn btn-primary m-1"');
+                        echo anchor('Home/index', 'Annuleren', 'class="btn btn-secondary m-1"');
+                    ?>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4"></div>
+    </div>
 </div>
