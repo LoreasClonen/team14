b75deb61e409b5cc12d0ca9fa31f4b1450e71975
Steven <r0709457@student.thomasmore.be>
2019-04-04 15:36:43 +0200

    probleem oplossen

--- project_BI_APP/application/views/main_menu.php
+++ project_BI_APP/application/views/main_menu.php
@@ -23,33 +23,26 @@
             <p>Welkom bij zwembad Kempenrust. Hier kan u terecht om makkelijk een zwemles te reserveren of om een
                 zwemfeestje te boeken.</p>
 
-<<<<<<< HEAD
-        </header>
+
     </div>
-=======
-    <?php echo smallDivAnchor('zwemlessen/keuze', 'Zwemles aanvragen', 'class="btn btn-primary"'); ?>
-    <?php echo smallDivAnchor('zwemfeestjes/zwemfeestjeBoeken', 'Zwemfeestje boeken', 'class="btn btn-primary"'); ?>
-    <?php echo smallDivAnchor('les1/valideer', 'Webshop', 'class="btn btn-primary"'); ?>
->>>>>>> aanmelden zwemfeestje bevestigingspagina en master
-</div>
-<div class="row">
-    <div class="col">
-        <div class="container-fluid">
+    <div class="row">
+        <div class="col">
+            <div class="container-fluid">
 
-            <?php echo smallDivAnchor('zwemlessen/keuze', 'Zwemles aanvragen', 'class="btn btn-primary"'); ?>
-            <?php echo smallDivAnchor('les1/toonTabs', 'Zwemfeestje boeken', 'class="btn btn-primary"'); ?>
-            <?php echo smallDivAnchor('les1/valideer', 'Webshop', 'class="btn btn-primary"'); ?>
+                <?php echo smallDivAnchor('zwemlessen/keuze', 'Zwemles aanvragen', 'class="btn btn-primary"'); ?>
+                <?php echo smallDivAnchor('zwemfeestjes/zwemfeestjeBoeken', 'Zwemfeestje boeken', 'class="btn btn-primary"'); ?>
+                <?php echo smallDivAnchor('les1/valideer', 'Webshop', 'class="btn btn-primary"'); ?>
 
+            </div>
+        </div>
+        <div class="col">
+            <div class="container-fluid text-center">
+                <h3 id="jquery" class="mt-4">Nieuwsberichten:</h3>
+                <div><?php
+
+                        echo $lijstBerichten;
+                    ?>
+                </div>
+            </div>
         </div>
     </div>
-<div class="col">
-<div class="container-fluid text-center">
-    <h3 id="jquery" class="mt-4">Nieuwsberichten:</h3>
-    <div><?php
-
-            echo $lijstBerichten;
-        ?>
-    </div>
-</div>
-</div>
-</div>
