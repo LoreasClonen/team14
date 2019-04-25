9debbf456902ca23573215ed61754646371500d5
SReggers <greenskyro@gmail.com>
2019-04-25 14:51:45 +0200

    help view zwemlessen aanmelden geschreven

---
+++ project_BI_APP/application/views/zwemlessen/help_aanmelden_zwemlessen.php
@@ -0,0 +1,43 @@
+<div>
+    <p>
+        Via dit formulier kan u voor uzelf of uw kinderen zwemlessen aanvragen.
+        Dit document overloopt stap voor stap deze procedure met bij elke stap wat extra uitleg.
+    </p>
+    <h4 id="header1">1: Selecteer een van de drie opties</h4>
+    <dl>
+        <dt>Voogd</dt>
+        <dd>Kies deze optie als u uw kind wilt inschrijven voor zwemlessen. Ga naar <a href="#header2.1">2.1</a>.</dd>
+        <dt>Mezelf</dt>
+        <dd>Kies deze optie als u uzelf wilt inschrijven voor zwemlessen. Ga naar <a href="#header2.2">2.2</a>.</dd>
+        <dt>Al klant?</dt>
+        <dd>Kies deze optie als u uw bestaande inschrijving wilt annuleren. Ga naar <a href="#header2.3">2.3</a>.</dd>
+    </dl>
+    <h4 id="header2.1">2.1: Voogd – Invulvelden</h4>
+    <p>
+        Hier vult u eerst uw eigen naam in bij naam voogd en uw email adres zodat wij u kunnen contacteren. Daarbuiten vult u ook de gegevens van uw kind in (naam, geboortedatum en zwemniveau).
+        Druk tenslotte op inschrijven en ga naar <a href="#header3">3</a>.
+    </p>
+    <h4 id="header2.2">2.2: Mezelf – Invulvelden</h4>
+    <p>
+        Hier vult u al uw gegevens in. Uw email en adres zullen allen gebruikt worden voor contact op te nemen wanneer dit noodzakelijk is.
+        Druk op inschrijven en ga naar <a href="#header3">3</a>.
+    </p>
+    <h4 id="header2.3">2.3: Inschrijving annuleren</h4>
+    <p>
+        Hier vult u uw gegevens in en kan u drukken op “Annuleren” om uw inschrijving definitief te annuleren.
+    </p>
+    <h4 id="header3">3: Keuze zwemlessen</h4>
+    <p>
+        Op deze pagina verschijnt nu een lijst van lesmomenten waaruit u kan kiezen. Indien u zeker bent dat u (of uw kind) op een of meerdere van deze momenten zou kunnen komen kunt u deze selecteren en op “Keuze bevestigen” drukken. Ga in dat geval naar
+        <a href="#header4.1">4.1</a>.
+        Kunt u op geen van deze momenten? Druk dan op “Inschrijving annuleren” en ga naar <a href="#header4.2">4.2</a>.
+    </p>
+    <h4 id="header4.1">4.1: Keuze bevestigen</h4>
+    <p>
+        U krijgt een bevestiging van de inschrijving.
+    </p>
+    <h4 id="header4.2">4.2: Annuleren</h4>
+    <p>
+        Hier kan u uw inschrijving definitief annuleren door op de “Bevestigen” knop te drukken, of terug naar uw inschrijving te gaan door op “Annuleren” te drukken.
+    </p>
+</div>
