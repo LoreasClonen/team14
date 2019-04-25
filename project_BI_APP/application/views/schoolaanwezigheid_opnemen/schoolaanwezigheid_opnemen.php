4b5d4af91ce4664a9eacf0b068cea754a2dc9865
sharinuyts <r0614779@student.thomasmore.be>
2019-04-25 12:38:25 +0200

    schoolaanwezigheid verder afwerken

--- project_BI_APP/application/views/schoolaanwezigheid_opnemen/schoolaanwezigheid_opnemen.php
+++ project_BI_APP/application/views/schoolaanwezigheid_opnemen/schoolaanwezigheid_opnemen.php
@@ -30,26 +30,6 @@
 
             echo '<div id="klassen"></div>';
 
-//
-//            echo form_label("Aantal zwemmers");
-//            $dataAantal = array('id' => 'leerlingenAantal',
-//                'name' => 'leerlingenAantal',
-//                'class' => 'form-control',
-//                'placeholder' => 'aantal zwemmers',
-//                'required' => 'required',
-//                'size' => '30');
-//            echo form_input($dataAantal);
-//
-//            $leerjaren = array();
-//            foreach ($klassen as $klas) {
-//                $leerjaren[$klas->id] = $klas->klasnaam;
-//
-//            }
-//            echo form_label("Klas");
-//            echo form_dropdown('klasnaam', $leerjaren, '--kies hier het leerjaar--', 'class="form-control dropdown"');
-//
-//
-
             echo form_submit(array("value" => "Opslaan", "class" => "btn btn-primary my-3", "id" => "schoolaanwezigheid_opslaan"));
 
             echo form_close();
