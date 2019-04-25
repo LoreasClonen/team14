f1ab5f60d58f70d8027a435b1f88b526b9b36cba
LoreasClonen <r0720792@student.thomasmore.be>
2019-04-25 15:06:53 +0200

    bevestiging toegevoegd

---
+++ project_BI_APP/application/views/zwemlessen/bevestigings_mail.php
@@ -0,0 +1,26 @@
+<div class="modal" tabindex="-1" role="dialog" id ="error">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Beste</p>
+                <p>Bedankt voor de inschrijving!</p>
+                <p>Dit is enkel een bevestigingsmail</p>
+            </div>
+            <div class="modal-footer">
+                <a class="btn btn-secondary" href=<?php echo site_url()?>> Home!</a>
+
+            </div>
+        </div>
+    </div>
+</div>
+<script type="text/javascript">
+    $(window).on('load',function(){
+        $('#error').modal('show');
+    });
+</script>
