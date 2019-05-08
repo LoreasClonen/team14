e7ad709aad2588e415490af595f27cf65bf98de7
sharinuyts <r0614779@student.thomasmore.be>
2019-05-08 12:48:43 +0200

    datum bij schoolaanwezigheid zichtbaar en aanpasbaar maken

--- project_BI_APP/application/views/schoolaanwezigheid_opnemen/ajax_klassenLijst.php
+++ project_BI_APP/application/views/schoolaanwezigheid_opnemen/ajax_klassenLijst.php
@@ -10,6 +10,17 @@
             'size' => '30');
         echo form_input($dataAantal);
 
+        // Geeft een invulveld voor de correcte datum
+        echo form_label('datumLes', 'datumLes');
+        $dataDatumLes = array('id' => 'datumLes',
+            'name' => 'datumLes',
+            'class' => 'form-control',
+            'placeholder' => 'dag/maand/jaar',
+            'required' => 'required',
+            'size' => '30',
+            'type' => 'date');
+        echo form_input($dataDatumLes);
+
         // Dropdown met de verschillende klassen per school
         echo form_label("Klassen", "klasId");
         $options = array();
@@ -23,5 +34,7 @@
             'class' => 'form-control');
 
         echo form_dropdown($dataKlassen, $options);
+
+
 
 
