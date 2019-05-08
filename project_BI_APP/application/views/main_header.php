d95b9308d0bbbc78c0c431e90bd24f236e7c5210
LoreasClonen <r0720792@student.thomasmore.be>
2019-05-08 14:52:19 +0200

    titles between links menu

--- project_BI_APP/application/views/main_header.php
+++ project_BI_APP/application/views/main_header.php
@@ -14,18 +14,30 @@
                       <li>  <?php echo anchor("Inloggen/meldAan", "Inloggen", "class = 'list-group-item list-group-item-action'");?> </li>
                 <?php    } else { // wel aangemeld ?>
                             <li class = "active"> <?php echo anchor('Inloggen/meldAf', 'Afmelden', "class = 'list-group-item list-group-item-action'");?> </li>
+                            <li>  <?php echo anchor("Inloggen/nieuwPaswoord", "Nieuw Wachtwoord", "class='list-group-item list-group-item-action'");?> </li>
+                            <hr>
+                            <li> <h6>zwemmers en lessen</h6></li>
+                            <hr>
                             <li>  <?php    echo anchor("Zwemmer/zwemmersOphalen", "Zwemmers", "class = 'list-group-item list-group-item-action'") ;?> </li>
                             <li>  <?php    echo anchor("Wachtlijst/getWachtlijsten", "Wachtlijst", "class = 'list-group-item list-group-item-action'");?> </li>
                             <li>  <?php    echo anchor("Zwemgroepen/zwemgroepenOphalen", "Zwemgroepen", "class = 'list-group-item list-group-item-action'");?> </li>
-                        <li>  <?php echo anchor("Scholen/aanwezighedenIngeven", "Aanwezigheden scholen", "class = 'list-group-item list-group-item-action'");?> </li>
-                        <li>  <?php echo anchor("Inloggen/nieuwPaswoord", "Nieuw Wachtwoord", "class='list-group-item list-group-item-action'");?> </li>
+                            <hr>
+                            <li><h6>scholen</h6></li>
+                            <hr>
+                            <li>  <?php echo anchor("Scholen/aanwezighedenIngeven", "Aanwezigheden scholen", "class = 'list-group-item list-group-item-action'");?> </li>
+
                     <?php    if ($gebruiker->isAdmin == 1) { ?>
 
                             <li>  <?php echo anchor("Scholen/toonScholen", "Scholen", "class = 'list-group-item list-group-item-action'");?> </li>
                             <li>  <?php echo anchor("Facturen/getScholen", "Schoolfacturen", "class = 'list-group-item list-group-item-action'");?> </li>
+                            <hr>
+                            <li><h6>zwemfeestjes</h6></li>
+                            <hr>
                             <li>  <?php echo anchor("Zwemfeestjes/getZwemfeestMomenten", "Zwemfeestjes", "class = 'list-group-item list-group-item-action'");?> </li>
+                            <hr>
+                            <li><h6>extra beheer</h6></li>
+                            <hr>
                             <li>  <?php echo anchor("Nieuwsberichten/nieuwsberichtenOphalen", "Nieuwsberichten", "class = 'list-group-item list-group-item-action'");?> </li>
-
                             <li>  <?php echo anchor("Gebruiker/getGebruikers", "Gebruikers", "class = 'list-group-item list-group-item-action'");?> </li>
                             <li>  <?php echo anchor("", "Aanpassingen", "class = 'list-group-item list-group-item-action'");?> </li>
                       <?php  }
