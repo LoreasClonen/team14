32f669e567e3e2c3f1513a871a058083f2c89fd2
LoreasClonen <r0720792@student.thomasmore.be>
2019-05-09 14:03:23 +0200

    added doxygen and added canceling

--- project_BI_APP/application/views/zwemlessen/bevestiging_annuleer_zwemles.php
+++ project_BI_APP/application/views/zwemlessen/bevestiging_annuleer_zwemles.php
@@ -7,16 +7,13 @@
                     Dankuwel!
                 </div>
                 <div class="card-body">
-                    <p class="card-text">Uw inschrijving is geannuleerd. We hebben u een bevestigingsmail gestuurd.</p>
+                    <p class="card-text">Uw inschrijving is geannuleerd.</p>
 
                 </div>
                 <div class="card-footer text-muted">
                     <?php echo anchor('Home/index', 'Hoofdmenu', 'class="btn btn-primary"'); ?>
                 </div>
             </div>
-        </div>
-        <div class="col-lg-4">
-            <p><?php echo anchor('Zwemlessen/emailBevestigingAnnuleerZwemles/' . $klantId, 'Gmail', 'class="btn btn-light"'); ?></p>
         </div>
     </div>
 </div>
--- project_BI_APP/application/views/zwemmers_beheren/overzicht_zwemmer.php
+++ project_BI_APP/application/views/zwemmers_beheren/overzicht_zwemmer.php
@@ -1,45 +1,80 @@
 <div class="col-12 mt-3">
-    <table class="table">
-        <thead>
-        <tr>
-            <th scope="col">Naam</th>
-            <th scope="col">Leeftijd</th>
-            <th scope="col">Niveau</th>
-            <th scope="col">Zwemgroep</th>
-
-        </tr>
-        </thead>
-        <tbody>
-
-                <tr>
-                    <td>
-                        <?php echo "<i class=\"fas fa-user\"></i> " . $zwemmer->voornaam . " " . $zwemmer->achternaam; ?>
-                    </td>
-                    <td>
-                        <?php
-                            $leeftijd = (int)date("Y m d") - (int)$zwemmer->geboortedatum;
-                            if ($leeftijd == date("Y")) {
-                                echo "/";
-                            } else {
-                                echo $leeftijd . " jaar";
-                            }
-                        ?>
-                    </td>
-                    <td>
-                        <?php echo $zwemniveau->niveauNaam ?>
-                    </td>
-                    <td>
-                        <?php
-                            foreach($zwemgroep as $zwemgroepNaam) {
-                                echo $zwemgroepNaam->groepsnaam . ' ';
-                            }
+    <?php
+                $attributes = array('name' => 'opslaan', 'id' => 'inschrijfformulier', 'role' => 'form');
+                form_open('zwemmer/updateZwemmer', $attributes);
+                echo "<hr />";
 
 
-                        ?>
-                    </td>
+                echo form_label('voornaam', 'voornaam');
+                $dataVoornaam = array('id' => 'voornaam',
+                    'name' => 'voornaam',
+                    'class' => 'form-control',
+                    'value' => $zwemmer->voornaam,
+                    'required' => 'required',
+                    'size' => '30');
+                echo form_input($dataVoornaam);
 
-                </tr>
+                echo form_label('achternaam', 'achternaam');
+                $dataAchternaam = array('id' => 'achternaam',
+                    'name' => 'achternaam',
+                    'class' => 'form-control',
+                    'value' => $zwemmer->achternaam,
+                    'required' => 'required',
+                    'size' => '30');
+                echo form_input($dataAchternaam);
 
-        </tbody>
-    </table>
+                echo form_label('email', 'email');
+                $dataEmail = array('id' => 'email',
+                    'name' => 'email',
+                    'class' => 'form-control',
+                    'value' => $zwemmer->email,
+                    'required' => 'required',
+                    'size' => '30');
+                echo form_input($dataEmail);
+
+                echo form_label('geboortedatum', 'geboortedatum');
+                    $dataGeboortedatum = array('id' => 'geboortedatum',
+                        'name' => 'geboortedatum',
+                        'class' => 'form-control',
+                        'value' => $zwemmer->geboortedatum,
+                        'required' => 'required',
+                        'size' => '30',
+                        'type' => 'date');
+                echo form_input($dataGeboortedatum);
+            //in 1 rij zetten
+            ?> <div class="row">
+                <div class="col-9">
+            <?php
+            echo form_label('straatnaam', 'straatnaam');
+            $dataStraatnaam = array('id' => 'straatnaam',
+                'name' => 'straatnaam',
+                'class' => 'form-control',
+                'value' => $zwemmer->straatnaam,
+                'required' => 'required',
+                'size' => '30');
+            echo form_input($dataStraatnaam);
+            ?> </div>
+                <div class="col-3">
+                    <?php
+            echo form_label('Huisnummer', 'huisnummer');
+            $dataHuisnummer = array('id' => 'huisnummer',
+                'name' => 'huisnummer',
+                'class' => 'form-control',
+                'value' => $zwemmer->huisnummer,
+                'required' => 'required',
+                'size' => '30');
+            echo form_input($dataHuisnummer);
+                    ?> </div>
+                </div> <?php
+            echo form_label('postcode', 'postcode');
+            $dataPostcode = array('id' => 'postcode',
+                'name' => 'postcode',
+                'class' => 'form-control',
+                'value' => $zwemmer->postcode,
+                'required' => 'required',
+                'size' => '30');
+            echo form_input($dataPostcode);
+            echo form_submit(array("value" => "Opslaan", "class" => "btn btn-primary my-3", "id" => "Opslaan"));
+            echo form_close();
+                ?>
 </div>
