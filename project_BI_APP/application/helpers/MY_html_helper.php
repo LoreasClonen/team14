9a6f5e3c367e4a654d8f28b88a5776b405a490a4
SReggers <greenskyro@gmail.com>
2019-05-09 16:30:10 +0200

    voeg geefVolledigeNaam toe aan helper

--- project_BI_APP/application/helpers/MY_html_helper.php
+++ project_BI_APP/application/helpers/MY_html_helper.php
@@ -25,3 +25,10 @@
     {
         return "<img src=\"" . geefVolledigeNaam($afbeelding) . "\"" . _stringify_attributes($attributen) . " />";
     }
+
+    function geefVolledigeNaam($afbeelding)
+    {
+        $CI =& get_instance();
+        $CI->load->helper('url');
+        return base_url() . str_replace('\\', '/', str_replace(FCPATH, "", APPPATH)) . $afbeelding;
+    }
