4444c05a6d6aeccd7eec39a02d10893f86efe809
SReggers <greenskyro@gmail.com>
2019-05-09 16:23:13 +0200

    voeg Doxygen toe aan helpers

--- project_BI_APP/application/helpers/MY_url_helper.php
+++ project_BI_APP/application/helpers/MY_url_helper.php
@@ -1,9 +1,25 @@
 <?php
+    /**
+     * functie divAnchor
+     * @brief maakt een knop
+     * @param $uri
+     * @param $attributes
+     * @param $title
+     * @return string
+     */
     function divAnchor($uri = '', $title = '', $attributes = '')
     {
         return "<div>" . anchor($uri, $title, $attributes) . "</div>\n";
     }
 
+    /**
+     * functie smallDivAnchor
+     * @brief maakt een kleine knop
+     * @param $uri
+     * @param $attributes
+     * @param $title
+     * @return string
+     */
     function smallDivAnchor($uri = '', $title = '', $attributes = '')
     {
         return "<div class='mt-1'>" .
